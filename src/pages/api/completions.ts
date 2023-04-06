8d9c1bcf249def57cbd0630cb0fd9c6112790907
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2023-04-06 14:46:25 +0100

    feat (completion): update simulated gpt response

--- src/pages/api/completions.ts
+++ src/pages/api/completions.ts
@@ -4,7 +4,7 @@ import { withApiAuthRequired } from "@auth0/nextjs-auth0";
 
 export default withApiAuthRequired(async function completions(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    res.send({ success: { message: "Thank you for using this service, open the console for a surpise!" } });
+    res.send({ success: { message: "Thank you for using this service, open the console for a surprise!" } });
   } else {
     res.status(405).send("method not supported");
   }
