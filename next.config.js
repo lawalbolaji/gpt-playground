40fa05c2dd5d031f7585b83feef67465603b21e7
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2023-05-02 12:28:59 +0100

    config: add profile image url to supported domains

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["via.placeholder.com"],
+    domains: ["via.placeholder.com", "s.gravatar.com"],
   },
   output: "standalone",
 };
