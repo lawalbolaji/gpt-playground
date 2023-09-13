794537bef088e24e5dd16e1d7b5e71312c9c6922
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2023-09-14 00:16:13 +0100

    analytics: add posthog analytics to track user events

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,23 @@ import { CacheProvider, EmotionCache } from "@emotion/react";
 import theme from "../config/theme";
 import createEmotionCache from "../config/createEmotionCache";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+// Check that PostHog is client-side
+if (
+  typeof window !== "undefined" &&
+  process.env.NEXT_PUBLIC_POSTHOG_KEY !== undefined &&
+  process.env.NEXT_PUBLIC_POSTHOG_HOST !== undefined
+) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -16,13 +33,15 @@ interface MyAppProps extends AppProps {
 
 export default function App({ Component, pageProps, emotionCache = clientSideEmotionCache }: MyAppProps) {
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <UserProvider>
-          <Component {...pageProps} />
-        </UserProvider>
-      </ThemeProvider>
-    </CacheProvider>
+    <PostHogProvider client={posthog}>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <UserProvider>
+            <Component {...pageProps} />
+          </UserProvider>
+        </ThemeProvider>
+      </CacheProvider>
+    </PostHogProvider>
   );
 }
